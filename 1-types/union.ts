4dba8211957f6cae7153c6a609c3b903c13162be
JYeonDev <whddus4837@gmail.com>
2022-05-05 23:58:21 +0900

    Union Type

---
+++ 1-types/union.ts
@@ -0,0 +1,33 @@
+{
+	// Union Types: OR
+	
+	type Direction = 'left' | 'right' | 'up' | 'down';
+	function move(direction: Direction) {
+		console.log(direction);
+	}
+	move('down');
+
+	type TileSize = 8 | 16 | 32;
+	const tile: TileSize = 16;
+
+	// 활용예제
+	// function: login => success, fail
+	type SuccessState = {
+		response: {
+			body: string;
+		};
+	}
+	type FailState = {
+		reason: string;
+	}
+	type LoginState = SuccessState | FailState;
+	function login(): LoginState {
+		return {
+			body: 'logged in!',
+		}
+	}
+	
+	// printLoginState(state)
+	// success => 성공! body
+	// fail => 실패! reason
+}
