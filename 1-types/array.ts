5e455632bd992ed0a54967e4d45fffee2850f125
JYeonDev <whddus4837@gmail.com>
2022-05-05 23:43:48 +0900

    Array

--- 1-types/array.ts
+++ 1-types/array.ts
@@ -1,1 +1,13 @@
-console.log('hi')
+{
+  // Array
+  const fruits: string[] = [];
+  const scores: Array<number> = [];
+
+  function printArray(fruits: readonly string[]) {
+		// readonly 데이터변경이 불가능하다
+  }
+
+	// Tuple
+	let student: [string, number];
+	student = ['name', 123];
+}
